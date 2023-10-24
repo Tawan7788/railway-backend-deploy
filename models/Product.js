f50b3934d455569138eb8891f52a56c0afdd2886
Tawan7788 <92264937+Tawan7788@users.noreply.github.com>
2023-10-24 16:27:18 +0700

    test

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
+    _id: false,
     prodName: String,
     prodDesc: String,
     prodPrice: Number,
-    updatedAt: { type: Date, default: Date.now }
+    updatedAt: { type: Date, default: Date.now },
+    __v: false,
 })
 
 module.exports = mongoose.model('Product', ProductSchema)
